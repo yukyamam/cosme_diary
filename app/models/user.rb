6e591c785b78aea28e967f9e6565f6a12e05dcba
yukyamam <yamamoto.yuka.0411@gmail.com>
2021-10-18 12:03:13 +0900

    DeviseGemの導入

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ApplicationRecord
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates :password, format: { with: VALID_PASSWORD_REGEX }
+
+  with_options presence: true do
+    validates :name
+    validates :birth
+  end
+
+end
