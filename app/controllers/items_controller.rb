732fc3db68f19d8d394270901f474a65ea5e29f2
yukyamam <yamamoto.yuka.0411@gmail.com>
2021-10-20 09:56:04 +0900

    items#destroyの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -30,6 +30,11 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def destroy
+    @item = Item.find(params[:id])
+    @item.destroy
+  end
+
   private
 
   def item_params
